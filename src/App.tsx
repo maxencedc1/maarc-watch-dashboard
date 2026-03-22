d90c70426ae2f19d361b7789e9610191592d1bbc
maxencedc1 <dechampsavin.maxence@gmail.com>
2026-03-22 17:57:08 +0100

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import Dashboard from './components/Dashboard';
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-slate-50">
-      <Dashboard />
-    </div>
-  );
-}
